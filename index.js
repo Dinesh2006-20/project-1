030311f9eb2c66c81f47784f5a9ecb9b7d1a1e9e
Dinesh2006-20 <dineshsneha29@gmail.com>
2024-12-26 21:43:27 +0530

    the file is modified

---
+++ index.js
@@ -0,0 +1,8 @@
+function func() {
+    const a=document.querySelector(".sidescreen");
+    a.style.display="block";
+}
+function func2() {
+    const b=document.querySelector(".sidescreen");
+    b.style.display="none";
+}
