3a0d6091c1f6d8254556b9e022bfbe5cd2336472
Dinesh2006-20 <dineshsneha29@gmail.com>
2024-12-27 06:32:03 +0530

    the file is updated

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ function func2() {
     const b=document.querySelector(".sidescreen");
     b.style.display="none";
 }
+
+function func4() {
+    const image=document.querySelector(".image-gallery")
+    const fashion=document.querySelector(".bt")
+    const grocery=document.querySelector(".Grocery")
+    grocery.style.backgroundColor="blue";
+    fashion.style.backgroundColor="white";
+    image.style.display="none";
+   
+}
+function func3() {
+    const image=document.querySelector(".image-gallery")
+    const grocery=document.querySelector(".Grocery")
+    const fashion=document.querySelector(".bt")
+    fashion.style.backgroundColor="blue";
+    grocery.style.backgroundColor="white";
+    image.style.display="block";
+}
