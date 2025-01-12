ded2b99c679ee1b5757cacd699d3b42e5aa04959
Dinesh2006-20 <dineshsneha29@gmail.com>
2025-01-13 03:00:43 +0530

    the file is updated

--- index.js
+++ index.js
@@ -11,34 +11,56 @@ function func4() {
     const image=document.querySelector(".image-gallery")
     const fashion=document.querySelector(".bt")
     const grocery=document.querySelector(".Grocery")
+    const female=document.querySelector(".Female")
+    const shoe=document.querySelector(".Shoe")
+    const radio=document.querySelector(".radio")
+    const items=document.querySelector(".grocery")
     grocery.style.backgroundColor="blue";
     fashion.style.backgroundColor="white";
     image.style.display="none";
+    female.style.display="none";
+    shoe.style.display='none';
+    radio.style.display="none";
+    items.style.display="block"
    
 }
 function func3() {
     const image=document.querySelector(".image-gallery")
+    const female=document.querySelector(".Female")
+    const shoe=document.querySelector(".Shoe")
+    const radio=document.querySelector(".radio")
     const grocery=document.querySelector(".Grocery")
+    const items=document.querySelector(".grocery")
     const fashion=document.querySelector(".bt")
     fashion.style.backgroundColor="blue";
     grocery.style.backgroundColor="white";
     image.style.display="block";
+    female.style.display="block";
+    shoe.style.display='block';
+    radio.style.display="block";
+    items.style.display="none"
 }
 function radiof() {
     const b=document.querySelector(".image-gallery");
     const a=document.querySelector(".Female")
+    const c=document.querySelector(".Shoe");
+    c.style.display="none";
     b.style.display="none";
-    a.style.display="block"
+    a.style.display="block";
 }
 function radiom() {
     const a=document.querySelector(".image-gallery");
     const b=document.querySelector(".Female")
+    const c=document.querySelector(".Shoe");
+    c.style.display="none";
     a.style.display="block";
     b.style.display="none";
 }
 function radios() {
     const a=document.querySelector(".image-gallery");
     const b=document.querySelector(".Female")
+    const d=document.querySelector(".Shoe")
+    d.style.display="block"
     a.style.display="none";
     b.style.display="none";
 }
